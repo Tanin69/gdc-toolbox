de7c2bf4527e4e95f8a6561acdc2450ee20676bd
oxypomme <oxytom8@gmail.com>
2022-05-01 12:51:13 +0200

    feat(nuxt): :goal_net: Added error page

--- nuxt-client/error.vue
+++ nuxt-client/error.vue
@@ -1,26 +1,46 @@
 <template>
-	<NuxtLayout name="default" header="Oops">
-		{{ props.error.message }}
-		<button @click="handleError">Clear errors</button>
-	</NuxtLayout>
+  <NuxtLayout name="default">
+    <div
+      class="w3-card-4 w3-padding w3-sand w3-display-middle"
+      style="opacity: 0.9"
+    >
+      <div style="text-align: center">
+        <img src="/assets/img/err.png" style="max-width: 20%; height: auto" />
+        <h1>{{ error.statusCode }}: {{ error.message }}</h1>
+        <p>
+          C'est sûrement de la faute
+          <spna class="gdc-text-strike">d'OxyTom</spna> de tanin...
+        </p>
+        <p>
+          Mais comme disait Raoul (d'après Sparfell) : "Il n’y a pas de fautes,
+          il n’y a que des erreurs, et les erreurs se corrigent."
+        </p>
+        <button class="w3-button gdc-color-tonic" @click="handleError">
+          Retourner au menu principal
+        </button>
+      </div>
+    </div>
+  </NuxtLayout>
 </template>
 
 <script lang="ts" setup>
-import background from "@/assets/img/backgrounds/error.jpg";
+import background from '@/assets/img/backgrounds/error.jpg'
 
-const runtimeConfig = useRuntimeConfig();
+const runtimeConfig = useRuntimeConfig()
 
-const props = defineProps({
-	error: Object,
-});
+const { error } = defineProps({
+  error: Object,
+})
 
-const handleError = () => clearError({ redirect: "/" });
+console.log({ ...error })
+
+const handleError = () => clearError({ redirect: '/' })
 
 // Defining meta tags
 useHead(() => ({
-	title: `${props.error.statusCode} | ${runtimeConfig.public.BASE_TITLE}`,
-	bodyAttrs: {
-		style: `background-image: url('${background}');`,
-	},
-}));
+  title: `${error.statusCode} | ${runtimeConfig.public.BASE_TITLE}`,
+  bodyAttrs: {
+    style: `background-image: url('${background}');`,
+  },
+}))
 </script>
--- nuxt-client/layouts/default.vue
+++ nuxt-client/layouts/default.vue
@@ -1,22 +1,23 @@
 <template>
-	<div>
-		<NavBar />
-		<div
-			class="w3-card-4 w3-padding w3-opacity w3-sand w3-hide-small w3-hide-medium"
-		>
-			<div style="text-align: center">
-				<h2>{{ props.header }}</h2>
-			</div>
-		</div>
-		<main>
-			<slot />
-		</main>
-		<FooterBar />
-	</div>
+  <div>
+    <NavBar />
+    <div
+      class="w3-card-4 w3-padding w3-opacity w3-sand w3-hide-small w3-hide-medium"
+      v-if="props.header"
+    >
+      <div style="text-align: center">
+        <h2>{{ props.header }}</h2>
+      </div>
+    </div>
+    <main>
+      <slot />
+    </main>
+    <FooterBar />
+  </div>
 </template>
 
 <script lang="ts" setup>
 const props = defineProps({
-	header: String,
-});
+  header: String,
+})
 </script>
