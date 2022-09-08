c3fb190dbc86f177cbd694f0cb6020e4772c36bf
oxypomme <oxytom8@gmail.com>
2022-09-08 12:06:42 +0200

    fix(api): :bug: fixed some API errors

--- api/app.js
+++ api/app.js
@@ -1,9 +1,8 @@
 //External modules imports
-const express = require('express');
-const exphbs = require('express-handlebars');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const pjson = require('./package.json');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const pjson = require("./package.json");
 const cors = require("cors");
 //App specific imports
 const routes = require("./routes");
@@ -18,29 +17,24 @@ const app = express();
 app.use(express.json());
 
 /* CORS config */
-app.use(cors({
-  origin: process.env.ALLOWED_CORS
-}));
+app.use(
+	cors({
+		origin: process.env.ALLOWED_CORS.split(","),
+	})
+);
 
 /* Database connection */
-mongoose.connect(process.env.DB_CONNECT,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log("MongoDB connection success"))
-  .catch(() => console.log("MongoDB connection failed !"));
+mongoose
+	.connect(process.env.DB_CONNECT, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
+	.then(() => console.log("MongoDB connection success"))
+	.catch(() => console.log("MongoDB connection failed !"));
 
-/* View engine instanciation and configuration */
-//express-handlerbars instanciation
-const hbs = exphbs.create({
-  extname: ".hbs",
-  partialsDir: __dirname + "/views/partials/",
-});
-//Inform express about template engine to use
-app.engine(".hbs", hbs.engine);
-app.set('view engine', '.hbs');
+/* Serve static assets (briefing images) */
+app.use(express.static("public"));
 
 //Connect all routes to application
 app.use("/", routes);
@@ -48,24 +42,25 @@ app.use("/", routes);
 /* Global error handling */
 //Catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error("Not Found");
+	err.status = 404;
+	next(err);
 });
 //Error handler
 app.use(function (err, req, res, next) {
-  //Send error message
-  res.status(err.status || 500).json({ "Error": err.message });
+	//Send error message
+	res.status(err.status || 500).json({ Error: err.message });
 });
 
 /* Launch the server app */
 try {
-  app.listen(process.env.PORT);
-  console.log(`${pjson.name}-${pjson.version} listening on port ${process.env.PORT}`);
-  console.log(`see ${pjson.repository.url} (author(s): ${pjson.author})`);
-}
-catch (e) {
-  console.error(e);
+	app.listen(process.env.PORT);
+	console.log(
+		`${pjson.name}-${pjson.version} listening on port ${process.env.PORT}`
+	);
+	console.log(`see ${pjson.repository.url} (author(s): ${pjson.author})`);
+} catch (e) {
+	console.error(e);
 }
 
 module.exports = app;
--- api/routes/index.js
+++ api/routes/index.js
@@ -3,15 +3,15 @@
  * This is the main route index.
  * Each endpoint delegates his code to a controller (with a few exceptions for very simple endpoints).
  * render delegated to a controller code must be called like that :
- * router.get|post|etc ("/api/.../...", controllerName.controllerFunction). 
- * 
+ * router.get|post|etc ("/api/.../...", controllerName.controllerFunction).
+ *
  */
 
 // External modules imports
 const express = require("express");
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
-const jwtAuthz = require('express-jwt-authz');
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const jwtAuthz = require("express-jwt-authz");
 //Controllers imports
 const missionListController = require("../controllers/missionListController");
 const missionAddController = require("../controllers/missionAddController");
@@ -22,17 +22,17 @@ const missionUpdateController = require("../controllers/missionUpdateController"
 /* Authentication */
 //Create middleware for checking the JWT
 const checkJwt = jwt({
-    // Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint
-    secret: jwksRsa.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `https://gdc-toolbox-api.eu.auth0.com/.well-known/jwks.json`
-    }),
-    //Validate the audience and the issuer
-    audience: process.env.AUTH0_API_AUDIENCE,
-    issuer: process.env.AUTH0_DOMAIN,
-    algorithms: ['RS256']
+	// Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint
+	secret: jwksRsa.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 5,
+		jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+	}),
+	//Validate the audience and the issuer
+	audience: process.env.AUTH0_AUDIENCE,
+	issuer: process.env.AUTH0_DOMAIN,
+	algorithms: ["RS256"],
 });
 
 const router = express.Router();
@@ -47,9 +47,19 @@ router.get("/api/mission/show/:missionPbo", missionShowController.showMission);
 router.post("/api/mission/check/", missionCheckController.checkMission);
 
 //Publishes a mission
-router.post("/api/mission/add/", checkJwt, jwtAuthz(['add:mission']), missionAddController.addMission);
+router.post(
+	"/api/mission/add/",
+	checkJwt,
+	jwtAuthz(["add:mission"]),
+	missionAddController.addMission
+);
 
 //Updates a mission
-router.put("/api/mission/update/:missionPbo", checkJwt,  jwtAuthz(['update:mission']), missionUpdateController.updateMission);
+router.put(
+	"/api/mission/update/:missionPbo",
+	checkJwt,
+	jwtAuthz(["update:mission"]),
+	missionUpdateController.updateMission
+);
 
 module.exports = router;
