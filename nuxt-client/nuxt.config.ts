62cafea7dee38ba4057cccf7ef93f130a911a3bc
oxypomme <oxytom8@gmail.com>
2023-05-17 17:10:55 +0200

    build(nuxt): :wrench: now using nuxt overloading env

--- nuxt-client/nuxt.config.ts
+++ nuxt-client/nuxt.config.ts
@@ -15,30 +15,18 @@ export default defineNuxtConfig({
   },
 
   runtimeConfig: {
-    MONGO_URL: process.env.MONGO_URL,
-    MONGO_NAME: process.env.MONGO_DB_NAME,
-    MONGO_COLLECTION: process.env.MONGO_COLLECTION_NAME,
-    PBO_MANAGER: process.env.PBO_MANAGER,
+    MONGO_URL: '',
+    MONGO_NAME: 'gdc',
+    MONGO_COLLECTION: 'missions',
+    PBO_MANAGER: '',
     UPLOAD_TEMP_DIR: resolve(process.env.TEMP_DIR!),
     MISSIONS_DIR: resolve(process.env.MISSIONS_DIR!),
     IMAGE_DIR: resolve(process.env.IMAGES_DIR!),
 
     public: {
       BASE_TITLE: 'GDC Toolbox',
-      /**
-       * @deprecated Use `/api/...` once migrated
-       */
-      API_BASE: process.env.API_BASE,
-      /**
-       * @deprecated Use `/api/mission` once migrated
-       */
-      API_MISSION_ENDPOINT: process.env.API_BASE + '/api/mission',
-      /**
-       * @deprecated Use `/api/...` once migrated
-       */
-      API_MISSION_IMAGE: process.env.API_BASE + '/img/brf',
-      AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-      AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+      AUTH0_DOMAIN: 'gdc.eu.auth0.com',
+      AUTH0_CLIENT_ID: '',
     },
   },
 })
