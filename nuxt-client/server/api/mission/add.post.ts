9794191217e7a786efc4fc1c79673d5db7165085
Morbakos <alexisjacob21@gmail.com>
2023-05-21 19:29:58 +0200

    Remove unused route

--- nuxt-client/server/api/mission/add.post.ts
+++
@@ -1,9 +0,0 @@
-import { Db } from 'mongodb'
-import finalizeMissionType from './helper';
-
-const { MONGO_COLLECTION } = useRuntimeConfig()
-
-export default defineEventHandler(async (event) => {
-    const body = await readBody(event)
-    console.log(body);
-})
